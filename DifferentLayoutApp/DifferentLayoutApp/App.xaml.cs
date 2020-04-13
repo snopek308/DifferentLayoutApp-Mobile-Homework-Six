6f25d6312186a251d221f10e45c06d6d903649fc
asnopek <asnopek486@gmail.com>
2020-04-13 13:13:40 -0500

    Realized I forgot to save before I pushed it, so the last couple of updates I had didn't work. Now they work.

--- DifferentLayoutApp/DifferentLayoutApp/App.xaml.cs
+++ DifferentLayoutApp/DifferentLayoutApp/App.xaml.cs
@@ -10,7 +10,7 @@ namespace DifferentLayoutApp
         {
             InitializeComponent();
 
-            MainPage = new MainPage();
+            MainPage = new NavigationPage(new MainPage());
         }
 
         protected override void OnStart()
