9f11a202fa08b5483bb51c84b527e0e2d89ab5df
asnopek <asnopek486@gmail.com>
2020-04-13 13:06:58 -0500

    Add project files.

---
+++ DifferentLayoutApp/DifferentLayoutApp/MainPage.xaml.cs
@@ -0,0 +1,40 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using Xamarin.Forms;
+
+namespace DifferentLayoutApp
+{
+    // Learn more about making custom code visible in the Xamarin.Forms previewer
+    // by visiting https://aka.ms/xamarinforms-previewer
+    [DesignTimeVisible(false)]
+    public partial class MainPage : ContentPage
+    {
+        public MainPage()
+        {
+            InitializeComponent();
+        }
+
+        private async void ShowGridPage(System.Object sender, System.EventArgs e)
+        {
+
+            await Navigation.PushAsync(new Grid());
+
+        }
+
+        private async void ShowPageThree(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new PageThree());
+        }
+
+        private async void ShowHelloMattPage(System.Object sender, System.EventArgs e)
+        {
+            await Navigation.PushAsync(new HelloMattPage());
+        }
+
+
+    }
+}
